9bfa767b43915b1fa14887bfe9e1e6808762f52b
Stuart Wong <swong31@gmail.com>
2020-08-07 13:22:52 -0700

    Dashboard now displays city name, temp, humidity and wind speed

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-var dashboardDiv = $("#dashboard");
+var mainDiv = $("#main");
+var main1Div = $("#main1");
+var main2Div = $("#main2");
+var main3Div = $("#main3");
+var main4Div = $("#main4");
 var searchBtn = $("#button-addon2");
 var searchInput = $("#searchInput");
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+// var queryUVI = 
 var apiKey = "&appid=84f82f417b1a12172cbd2e8e805152c8";
 
 searchBtn.click(function () {
@@ -16,10 +21,25 @@ searchBtn.click(function () {
     // Test to see what I'm retrieving from OWM
     console.log(result);
     // Variable to store temp
-    var temp = (result.main.temp - 273.15) * 1.8 + 32
-    dashboardDiv.text(temp.toFixed(2))
-    var pTag = $("<p>")
-    pTag.text("Humidity: " + result.main.humidity + "%")
-    dashboardDiv.append(pTag)
+    var temp = (result.main.temp - 273.15) * 1.8 + 32;
+    var h2Tag = $("<h2>");
+    var pTag = $("<p>");
+    var p2Tag = $("<p>");
+    var p3Tag = $("<p>");
+    var p4Tag = $("<p>");
+
+    h2Tag.text(result.name);
+    mainDiv.append(h2Tag);
+
+    pTag.text("Current Temperature: " + temp.toFixed(2) + " \xB0" + "F");
+    main1Div.append(pTag);
+
+    p2Tag.text("Humidity: " + result.main.humidity + "%");
+    main2Div.append(p2Tag);
+
+    p3Tag.text("Wind Speed: " + result.wind.speed + " MPH");
+    main3Div.append(p3Tag);
+
+    // p4Tag.text()
   });
 });
